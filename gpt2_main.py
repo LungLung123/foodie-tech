72812b436e59bd61ce2400f499698246fd7e1d95
LungLung123 <palang.anivan@gmail.com>
2020-07-26 19:02:21 +1000

    add model download

--- gpt2_main.py
+++ gpt2_main.py
@@ -4,6 +4,11 @@ import requests
 
 model_name = "124M"
 
+if not os.path.isdir(os.path.join("models", model_name)):
+	print(f"Downloading {model_name} model...")
+	gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
+
+
 sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess, model_name=model_name)
 
