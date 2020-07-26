f309c3746c6fba22bbc545aef5fce8a029898ab8
LungLung123 <palang.anivan@gmail.com>
2020-07-26 19:31:46 +1000

    update parameter

--- gpt2_main.py
+++ gpt2_main.py
@@ -12,12 +12,12 @@ if not os.path.isdir(os.path.join("models", model_name)):
 sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess, model_name=model_name)
 
-print("Model Loaded")
+print("Come try our coffee for")
 
 prompt = "Hello World!"
 
 print("Generating...")
 
-gpt2_text = gpt2.generate(sess, model_name=model_name, length=128, prefix=prompt, batch_size=3, nsamples=6, include_prefix=True, return_as_list=True)[0]
+gpt2_text = gpt2.generate(sess, model_name=model_name, temperature=1.0, length=50, prefix=prompt, batch_size=20, nsamples=20, include_prefix=True, return_as_list=True)[0]
 
 print(gpt2_text)
